233420a6585f06250b53aa4aadd1dde644c2a3ee
AnaySharmaCEO <gamingxjanata@gmail.com>
2024-11-30 23:34:42 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ['.C:\Users\beast\Downloads\Work\minproject\landing page\\**\\*.{html,js}.{html,js}'], // Update this path based on your project structure
+    theme: {
+      extend: {},
+    },
+    plugins: [
+      require('tailwindcss-text-stroke'),
+    ],
+  };
